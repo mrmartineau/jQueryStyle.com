6bac632508f3ead9531d1b4ff82e551e3cd6c953
Zander Martineau <zander@martineau.tv>
2012-10-24 01:36:07 +0100

    add config file

---
+++ textpattern/config.php
@@ -0,0 +1,9 @@
+<?php
+$txpcfg['db'] = 'jqs';
+$txpcfg['user'] = 'root';
+$txpcfg['pass'] = 'root';
+$txpcfg['host'] = 'localhost';
+$txpcfg['table_prefix'] = '';
+$txpcfg['txpath'] = '/textpattern';
+$txpcfg['dbcharset'] = 'utf8';
+?>
